4548c53911566f56d4134f2e61193df7e2b7ab12
agaman <ali.aka.tzoky@gmail.com>
2014-07-04 17:29:35 +0300

    Initial commit
    Added parent project
    Added EJB sample project

---
+++ projectParent/remote-ejb/server/src/main/java/ro/gboss/remoteEjb/server/SampleRemoteBean.java
@@ -0,0 +1,6 @@
+package ro.gboss.remoteEjb.server;
+
+public interface SampleRemoteBean
+{
+	public String echoWhatYouSaid(String s);
+}
---
+++ projectParent/remote-ejb/server/src/main/java/ro/gboss/remoteEjb/server/SampleRemoteBeanImpl.java
@@ -0,0 +1,19 @@
+package ro.gboss.remoteEjb.server;
+
+import javax.ejb.Remote;
+import javax.ejb.Stateless;
+
+@Stateless
+@Remote(SampleRemoteBean.class)
+public class SampleRemoteBeanImpl implements SampleRemoteBean 
+{
+
+	@Override
+	public String echoWhatYouSaid(String s) {
+		System.out.println("Echo bean method called...");
+		System.out.println("Returning the following string : ");
+		System.out.println("You said " + s);
+		return "You said : " + s;
+	}
+
+}
